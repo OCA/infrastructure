8fb5a5b62392ae7176edb1f7d0d6c7f15e62cb8d
noah <234082230@qq.com>
2016-08-29 10:54:35 +0800

    [ADD] connector_dns
    [FIX] format
    
    [IMP] format
    
    [IMP] unit test
    
    [IMP] unit test

---
+++ connector_dns/tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 Elico Corp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import common
+from . import test_backend
+from . import test_binder
