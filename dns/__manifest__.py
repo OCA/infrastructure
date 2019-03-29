f080d40c0147022380e02d3e0db7b8ccc575c3b3
Ruter Lv <ruter.lv@elico-corp.com>
2019-03-29 11:17:08 +0800

    add copyright

--- dns/__manifest__.py
+++ dns/__manifest__.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+# © 2019 Elico Corp (https://www.elico-corp.com).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'DNS Base',
     'category': 'connector',
