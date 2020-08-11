3d337edc5aae89698e60d14f16375cb67122dfc8
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2020-08-11 14:57:18 +0530

    [IMP] Improved code

--- connector_openshift/__manifest__.py
+++ connector_openshift/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Openshift Connector",
-    "summary": """Openshift Connector""",
+    "summary": """Red Hat OpenShift Integrations with Odoo.""",
     "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Odoo Community Association (OCA), Open Source Integrators",
@@ -12,6 +12,7 @@
     "category": "Tools",
     "depends": ["connector"],
     "data": ["security/ir.model.access.csv", "views/backend_openshift_view.xml"],
+    "external_dependencies": {"bin": ["oc"]},
     "installable": True,
     "maintainers": ["max3903"],
     "development_status": "Beta",
